9563fcdea70ccb49c7c37180598801aa1b1e3132
hoizzy <dbrtyu@naver.com>
2023-06-17 00:51:45 +0900

    add code for nickname

--- src/server.js
+++ src/server.js
@@ -17,17 +17,21 @@ const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done) => {
+        socket["nickname"]="Anon";
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+        socket.rooms.forEach((room) => {
+            socket.to(room).emit("bye", socket.nickname)
+        });
     })
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     })
+    socket.on("nickname", nickname => (socket["nickname"] = nickname));
 });
 
 
